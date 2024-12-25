f6916fec152e18179677378306ed5a09644edfa3
raysachin <raysachin2002@gmail.com>
2024-12-26 00:50:05 +0530

    model data

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ dotenv.config({ path: "./config.env" });
 const cors = require("cors");
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true
 }));
